5b99d0ab34b796d145c32c9dc886680d7c422bca
Kaloyan <k_volodiev@abv.bg>
2018-11-20 20:20:43 +0100

    - moved all the middlewares in a separate folder  - implemented register, login and vrify routes

--- app.js
+++ app.js
@@ -7,46 +7,46 @@ const upload = multer();
 const cron = require("cron").CronJob;
 const bcrypt = require("bcrypt");
 const Sequelize = require("sequelize");
-const validator = require('validator');
+const middleware = require("./middleware/middleware");
 
 const sequelize = new Sequelize("auth_service", "root", "", {
-	host: "localhost",
-	dialect: "mysql",
-	operatorsAliases: false,
-	pool: {
-		max: 5,
-		min: 0,
-		idle: 10000
-	}
+    host: "localhost",
+    dialect: "mysql",
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
 });
 
 const Users = sequelize.define(
-	"users",
-	{
-		id: {
-			type: Sequelize.INTEGER,
-			autoIncrement: true,
-			primaryKey: true
-		},
-		first_name: {
-			type: Sequelize.STRING
-		},
-		last_name: {
-			type: Sequelize.STRING
-		},
-		email: {
-			type: Sequelize.STRING,
-			validate: {
-				isEmail: true
-			}
-		},
-		password: {
-			type: Sequelize.STRING
-		}
-	},
-	{
-		freezeTableName: true // Model tableName will be the same as the model name
-	}
+    "users",
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        first_name: {
+            type: Sequelize.STRING
+        },
+        last_name: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: Sequelize.STRING
+        }
+    },
+    {
+        freezeTableName: true // Model tableName will be the same as the model name
+    }
 );
 
 const app = express();
@@ -64,144 +64,110 @@ app.use(body_parser.json());
 app.use(upload.array());
 app.use(express.static("public"));
 
-app.get("/", (request, responce) => {});
-
-//test register
-app.post("/register", (request, responce) => {
-	Users.findOrCreate({
-		where: { email: "asd1@asd.com" },
-		defaults: { first_name: "use1", last_name: "las1", password: "asdqawf" }
-	}).spread(function(user, created) {
-		console.log(
-			user.get({
-				plain: true
-			})
-		);
-		console.log(created);
-	});
+app.get("/", (request, responce) => {
+    responce.sendStatus(200);
 });
 
-app.post("/generate", extractUser, (request, responce) => {
-	jwt.sign(
-		{ user: request.user },
-		key,
-		{ algorithm: "ES256", expiresIn: "1d" },
-		(error, token) => {
-			if (error) {
-				logError({ function: "jwt.sign", error: error });
-				responce.sendStatus(403);
-			}
-			responce.json({ token: token });
-		}
-	);
+app.post("/register", middleware.register, (request, responce) => {
+    Users.findOrCreate({
+        where: { email: request.body.email },
+        defaults: { first_name: request.body.first_name, last_name: request.body.last_name, password: request.body.password }
+    }).spread(function(user, created) {
+        if (created) {
+            jwt.sign({ user: user.get() }, key, (error, token) => {
+                if (error) {
+                    responce.status(400).send({ error: "Could not sign JWT" });
+                } else {
+                    //deleting the following keys because could not find in the
+                    //documentation how to exclude them in the return object from the db
+                    delete user.get().id;
+                    delete user.get().password;
+                    delete user.get().createdAt;
+                    delete user.get().updatedAt;
+
+                    responce.json({
+                        user: user.get({ plain: true }),
+                        token: token
+                    });
+                }
+            });
+        } else {
+            responce.json({ message: "already excists" });
+        }
+    });
 });
 
-app.post("/verify", verifyToken, (request, responce) => {
-	jwt.verify(request.token, key, (error, token_data) => {
-		if (error) {
-			logError({ function: "jwt.verify", error: error });
-			responce.sendStatus(403);
-		} else {
-			responce.json({
-				data: token_data
-			});
-		}
-	});
+app.post("/login", middleware.login, (request, responce) => {
+    Users.findOne({ where: { email: request.body.email } }).then(user => {
+        // project will be the first entry of the Projects table with the title 'aProject' || null
+        if (user !== null) {
+            bcrypt.compare(request.body.password, user.password, function(error_hash, result_hash) {
+                if (error_hash) {
+                    responce.status(400).send({ error: "could not de-hash the password" });
+                } else {
+                    if (result_hash) {
+                        jwt.sign({ user: user }, key, (error, token) => {
+                            if (error) {
+                                responce.status(400).send({ error: "could not sign JWT" });
+                            } else {
+                                //deleting the following keys because could not find in the
+                                //documentation how to exclude them in the return object from the db
+                                delete user.get().password;
+
+                                responce.json({
+                                    user: user.get({ plain: true }),
+                                    token: token
+                                });
+                            }
+                        });
+                    } else {
+                        responce.status(400).send({ error: "incorect password" });
+                    }
+                }
+            });
+        } else {
+            responce.status(400).send({ error: "No such user" });
+        }
+    });
 });
 
-/**
- * middleware
- * @param {Request} request object
- * @param {Response} responce object
- * @param {Function} next callback function
- */
-function extractUser(request, responce, next) {
-	//checking if there are the necessary credentials in the body
-	if (
-		request.body.user_key !== undefined &&
-		request.body.user_name !== undefined
-	) {
-		request.user = {
-			user_key: request.body.user_key,
-			user_name: request.body.user_name
-		};
-		next();
-	} else {
-		logError({
-			function: "extractUser",
-			error: {
-				name: "UserCredentialsError",
-				message: "no_creadentials_found"
-			}
-		});
-		responce.sendStatus(403);
-	}
-}
-
-/**
- * middleware
- * @param {Request} request request object
- * @param {Response} responce responce object
- * @param {Function} next callback function
- */
-function verifyToken(request, responce, next) {
-	//getting the auth header
-	const bearer_header = request.headers["authorization"];
-
-	if (bearer_header !== undefined) {
-		//Token Syntax: Bearer <JWT>
-		const bearer = bearer_header.split(" ");
-		//getting the token
-		const bearer_token = bearer[1];
-		//attaching it to the request
-		request.token = bearer_token;
-		//continue
-		next();
-	} else {
-		logError({
-			function: "verifyToken",
-			error: { name: "VerificationFailed", message: "no_token_found" }
-		});
-		responce.sendStatus(403);
-	}
-}
-
-/**
- * used to log all the errors occured in the service
- * @param {JSON} error object representing the name of the error and the message
- */
-function logError(error) {
-	const timestamp = new Date();
-	const line = timestamp + ": " + JSON.stringify(error) + "\r\n";
-	fs.appendFile("log.txt", line, function(error_write, data) {});
-}
+app.post("/verify", middleware.verifyToken, (request, responce) => {
+    jwt.verify(request.token, key, (error, token_data) => {
+        if (error) {
+            responce.status(400).send({ error: "could not verify JWT" });
+        } else {
+            responce.json({
+                data: token_data
+            });
+        }
+    });
+});
 
 /**
  * generating new secret key to sign all the tokens with
  */
 function generateSignKey() {
-	let key = "";
-	const date = new Date().getTime();
-	const char_list =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+/?.,<>";
+    let key = "";
+    const date = new Date().getTime();
+    const char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+/?.,<>";
 
-	for (var i = 0; i < 250; i++) {
-		key += char_list.charAt(Math.floor(Math.random() * char_list.length));
-	}
-	return key + date;
+    for (var i = 0; i < 250; i++) {
+        key += char_list.charAt(Math.floor(Math.random() * char_list.length));
+    }
+    return key + date;
 }
 
 //running a cronjob every day at 00:00 to generate new key
 new cron(
-	"0 0 0 * * *",
-	function() {
-		key = generateSignKey();
-	},
-	null,
-	true,
-	"Europe/Amsterdam"
+    "0 0 0 * * *",
+    function() {
+        key = generateSignKey();
+    },
+    null,
+    true,
+    "Europe/Amsterdam"
 );
 
 app.listen(5001, () => {
-	console.log("Auth service running...");
+    console.log("Auth service running...");
 });
---
+++ middleware/middleware.js
@@ -0,0 +1,119 @@
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+const blacklist_sequence = "[\\\"<>=;:'`~,/?{}[]|()*&^%$#!]";
+
+module.exports = {
+    register,
+    login,
+    verifyToken: verifyToken
+};
+
+function register(request, responce, next) {
+    const required_keys = ["first_name", "last_name", "password", "email"];
+
+    let has_error = false;
+
+    for (const key of required_keys) {
+        console.log("qwe:" + request.body[key]);
+        if (request.body[key] && !has_error) {
+            if (!validator.isEmpty(request.body[key])) {
+                request.body[key] = validator.blacklist(request.body[key], blacklist_sequence);
+                if (key === "email") {
+                    if (!validator.isEmail(request.body[key])) {
+                        responce.status(400).send({ error: "email param not an email", field: key });
+                        has_error = true;
+                        break;
+                    }
+                }
+            } else {
+                responce.status(400).send({ error: "empty param", field: key });
+                has_error = true;
+                break;
+            }
+        } else {
+            responce.status(400).send({ error: "missing param", field: key });
+            has_error = true;
+            break;
+        }
+    }
+
+    if (!has_error) {
+        const salt_rounds = 10;
+        bcrypt.genSalt(salt_rounds, function(error, salt) {
+            if (error) {
+                responce.status(400).send({ error: "error generating salt" });
+            } else {
+                bcrypt.hash(request.body.password, salt, function(error_hash, hash) {
+                    if (error_hash) {
+                        responce.status(400).send({ error: "error generating hash" });
+                    } else {
+                        request.body.password = hash;
+                        next();
+                    }
+                });
+            }
+        });
+    }
+}
+
+/**
+ * middleware
+ * @param {Request} request object
+ * @param {Response} responce object
+ * @param {Function} next callback function
+ */
+function login(request, responce, next) {
+    //checking if there are the necessary credentials in the body
+    const required_keys = ["password", "email"];
+
+    const has_error = false;
+
+    for (const key of required_keys) {
+        if (!has_error && request.body[key]) {
+            if (!validator.isEmpty(request.body[key])) {
+                request.body[key] = validator.blacklist(request.body[key], blacklist_sequence);
+            } else {
+                responce.status(400).send({ error: "empty param", field: key });
+                has_error = true;
+                break;
+            }
+        } else {
+            responce.status(400).send({ error: "missing param", field: key });
+            has_error = true;
+            break;
+        }
+    }
+
+    if (!has_error) {
+        next();
+    }
+}
+
+/**
+ * middleware
+ * @param {Request} request request object
+ * @param {Response} responce responce object
+ * @param {Function} next callback function
+ */
+function verifyToken(request, responce, next) {
+    //getting the auth header
+    const bearer_header = request.headers["authorization"];
+
+    if (bearer_header !== undefined) {
+        //Token Syntax: Bearer <JWT>
+        const bearer = bearer_header.split(" ");
+        //getting the token
+        const bearer_token = bearer[1];
+        //attaching it to the request
+        request.token = bearer_token;
+        //continue
+        next();
+    } else {
+        logError({
+            function: "verifyToken",
+            error: { name: "VerificationFailed", message: "no_token_found" }
+        });
+        responce.sendStatus(403);
+    }
+}
